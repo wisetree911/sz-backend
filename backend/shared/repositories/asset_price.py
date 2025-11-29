d220fed086d3a52a98b7e3b0f30d8110579e9d59
wisetree911 <gariksharik224@gmail.com>
2025-11-29 21:44:49 +0500

    refactor: rewrite asset_price repository to use instance-based session

--- backend/shared/repositories/asset_price.py
+++ backend/shared/repositories/asset_price.py
@@ -1,22 +1,26 @@
 from sqlalchemy import select
 from shared.models.asset_price import AssetPrice
 from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession
+
 class AssetPriceRepository:
-    @staticmethod
-    async def get_by_id(session, asset_id: int):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(self, asset_id: int):
         query = select(AssetPrice).where(AssetPrice.asset_id == asset_id)
-        result = await session.execute(query)
+        result = await self.session.execute(query)
         asserts = result.scalars().all()
         return asserts
     
     @staticmethod
-    async def get_all(session):
+    async def get_all(self):
         query = select(AssetPrice)
-        result = await session.execute(query)
+        result = await self.session.execute(query)
         return result.scalars().all()
     
     @staticmethod
-    async def create(session,
+    async def create(self,
             asset_id,
             price,
             currency,
@@ -27,15 +31,15 @@ class AssetPriceRepository:
             currency=currency,
             source=source
         )
-        session.add(new_price)
-        await session.commit()
-        await session.refresh(new_price)
+        self.session.add(new_price)
+        await self.session.commit()
+        await self.session.refresh(new_price)
         return new_price
     
     @staticmethod
-    async def get_prices_since(session, ids: list[int], since: datetime):
+    async def get_prices_since(self, ids: list[int], since: datetime):
         if not ids: return []
         query = (select(AssetPrice).where(AssetPrice.asset_id.in_(ids), AssetPrice.timestamp >= since))
-        prices = await session.execute(query)
+        prices = await self.session.execute(query)
         return prices.scalars().all()
     
