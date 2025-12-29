61876fb0a46a14e2acfbd89d82b5103d452128ea
wisetree911 <gariksharik224@gmail.com>
2025-12-29 20:29:02 +0500

    refactor: опечатка get_porfolio_service -> get_portfolio_service

--- backend/app/api/routers/adm/portfolios.py
+++ backend/app/api/routers/adm/portfolios.py
@@ -1,30 +1,30 @@
 from fastapi import APIRouter, status, Depends
 from app.schemas.portfolio import PortfolioCreateAdm, PortfolioResponseAdm, PortfolioUpdateAdm
 from app.services.portfolios import PortfolioService
-from app.api.deps import get_porfolio_service
+from app.api.deps import get_portfolio_service
 router = APIRouter(prefix="/portfolios", tags=["Portfolios"])
 
 @router.get("/{portfolio_id}")
-async def get_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def get_by_id(portfolio_id: int, service: PortfolioService=Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.get_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
 
 @router.get("/")
-async def get_all(service: PortfolioService=Depends(get_porfolio_service)) -> list[PortfolioResponseAdm]:
+async def get_all(service: PortfolioService=Depends(get_portfolio_service)) -> list[PortfolioResponseAdm]:
     return await service.get_all_portfolios()
 
 @router.post("/")
-async def create(payload: PortfolioCreateAdm, service: PortfolioService = Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def create(payload: PortfolioCreateAdm, service: PortfolioService = Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.create_portfolio(payload)
 
 @router.delete("/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)):
+async def delete_by_id(portfolio_id: int, service: PortfolioService=Depends(get_portfolio_service)):
     await service.delete_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
     return
 
 @router.patch("/{portfolio_id}")
-async def update_portfolio(portfolio_id: int, payload: PortfolioUpdateAdm, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def update_portfolio(portfolio_id: int, payload: PortfolioUpdateAdm, service: PortfolioService=Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.update(portfolio_id=portfolio_id, payload=payload)
 
 @router.get("/user/{user_id}") # add pydantic
-async def get_user_portfolios(user_id: int, service: PortfolioService=Depends(get_porfolio_service)):
+async def get_user_portfolios(user_id: int, service: PortfolioService=Depends(get_portfolio_service)):
     return await service.get_user_portfolios(user_id=user_id)
--- backend/app/api/routers/public/portfolios.py
+++ backend/app/api/routers/public/portfolios.py
@@ -1,28 +1,28 @@
 from fastapi import APIRouter, status, Depends
 from app.schemas.portfolio import PortfolioCreatePublic, PortfolioResponseAdm, PortfolioUpdateAdm, PortfolioUpdatePublic
 from app.services.portfolios import PortfolioService
-from app.api.deps import get_porfolio_service
+from app.api.deps import get_portfolio_service
 from app.api.deps import get_current_user
 
 router = APIRouter(prefix="/portfolios", tags=["Portfolios"])
 
 @router.get("/")
-async def get_portfolios(current_user=Depends(get_current_user), service: PortfolioService=Depends(get_porfolio_service)) -> list[PortfolioResponseAdm]:
+async def get_portfolios(current_user=Depends(get_current_user), service: PortfolioService=Depends(get_portfolio_service)) -> list[PortfolioResponseAdm]:
     return await service.get_user_portfolios(user_id=current_user.id)
 
 @router.get("/{portfolio_id}")
-async def get_by_portfolio_id(portfolio_id:int, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def get_by_portfolio_id(portfolio_id:int, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.get_portfolio_for_user(portfolio_id=portfolio_id, user_id=current_user.id)
 
 @router.delete("/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_by_id(portfolio_id: int, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_porfolio_service)):
+async def delete_by_id(portfolio_id: int, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_portfolio_service)):
     await service.delete_portfolio_for_user(portfolio_id=portfolio_id, user_id=current_user.id)
     return None
 
 @router.post("/")
-async def create_portfolio_for_user(payload: PortfolioCreatePublic, current_user=Depends(get_current_user), service: PortfolioService = Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def create_portfolio_for_user(payload: PortfolioCreatePublic, current_user=Depends(get_current_user), service: PortfolioService = Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.create_portfolio_for_user(payload=payload, user_id=current_user.id)
 
 @router.patch("/{portfolio_id}")
-async def update_portfolio_for_user(portfolio_id: int, payload: PortfolioUpdatePublic, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponseAdm:
+async def update_portfolio_for_user(portfolio_id: int, payload: PortfolioUpdatePublic, current_user=Depends(get_current_user), service: PortfolioService=Depends(get_portfolio_service)) -> PortfolioResponseAdm:
     return await service.update_for_user(portfolio_id=portfolio_id, user_id=current_user.id, payload=payload)
