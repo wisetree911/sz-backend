961e787894ffd7925020b254d070f1ce09afc616
wisetree911 <gariksharik224@gmail.com>
2026-01-02 19:09:54 +0500

    migr: мигрировал новую таблицу в бд для рефреш токенов и добавил в енв алмебика

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -13,23 +13,20 @@ from shared.models.asset import Asset
 from shared.models.asset_price import AssetPrice
 from shared.models.portfolio import Portfolio
 from shared.models.trade import Trade
+from shared.models.refresh_sessions import RefreshSession
 
 
 config = context.config
 
-# Подтягиваем конфиг логирования из alembic.ini
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Пробрасываем строку подключения
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
-# метаданные всех таблиц
 target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Запуск миграций в офлайн-режиме"""
     context.configure(
         url=settings.DATABASE_URL,
         target_metadata=target_metadata,
@@ -42,7 +39,6 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    """Выполняем миграции в обычном режиме"""
     context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
@@ -50,7 +46,6 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    """Async миграции"""
     connectable = create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
