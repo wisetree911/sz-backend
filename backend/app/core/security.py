b955ccf442f03cc99c340aef5100d09f8bfb70b2
wisetree911 <gariksharik224@gmail.com>
2026-01-03 19:27:52 +0500

    feat: create_refresh_token для создания рефреш токена

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -13,5 +13,21 @@ def verify_password(password: str, hashed: str) -> bool:
 
 def create_access_token(user_id: int) -> str:
     expirates_at = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload = {"sub": user_id, "exp": expirates_at}
+    payload = {
+        "sub": str(user_id), 
+        "type": "access",
+        "exp": expirates_at,
+        "iat": datetime.now(timezone.utc)
+        }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def create_refresh_token(user_id: int, jti: str) -> str:
+    expires_at = datetime.now(timezone.utc) + timedelta(days=30)
+    payload = {
+        "sub": str(user_id),
+        "jti": jti,
+        "type": "refresh",
+        "exp": expires_at,
+        "iat": datetime.now(timezone.utc)
+    }
     return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
