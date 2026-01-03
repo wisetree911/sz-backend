055ed9fd0e6df221558e1d889ba524d6bdcdebdd
wisetree911 <gariksharik224@gmail.com>
2026-01-03 19:25:19 +0500

    feat: добавил переменную для жизни рефреш токена

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
 
     class Config:
         env_file = ".env"
