22562fcc9350fe3f3675e848b8d7ed98288776b0
wisetree911 <gariksharik224@gmail.com>
2026-01-28 05:23:04 +0500

    refactor: описания к ручками assets

--- backend/app/api/routers/public/assets.py
+++ backend/app/api/routers/public/assets.py
@@ -6,21 +6,33 @@ from fastapi import APIRouter, Depends
 router = APIRouter(prefix='/assets', tags=['Assets'])
 
 
-@router.get('/{asset_id}', response_model=AssetResponsePublic)
+@router.get(
+    '/{asset_id}',
+    response_model=AssetResponsePublic,
+    summary='Информация об активе.',
+)
 async def get_asset_by_id(
     asset_id: int, service: AssetService = Depends(get_asset_service)
 ) -> AssetResponsePublic:
     return await service.get_by_id(asset_id=asset_id)
 
 
-@router.get('/by-ticker/{ticker}', response_model=AssetResponsePublic)
+@router.get(
+    '/by-ticker/{ticker}',
+    response_model=AssetResponsePublic,
+    summary='Информация об активе по тикеру.',
+)
 async def get_asset_by_ticker(
     ticker: str, service: AssetService = Depends(get_asset_service)
 ) -> AssetResponsePublic:
     return await service.get_by_ticker(ticker=ticker)
 
 
-@router.get('/', response_model=list[AssetResponsePublic])
+@router.get(
+    '/',
+    response_model=list[AssetResponsePublic],
+    summary='Информация обо всех существующих активах.',
+)
 async def get_assets(
     service: AssetService = Depends(get_asset_service),
 ) -> list[AssetResponsePublic]:
